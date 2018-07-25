bf1d766f239f6ede1bccb475150f84a9e49a24e9
rvirji <rvirji@vendasta.com>
2018-07-25 15:15:01 -0600

    Add va-icon example

---
+++ src/material-examples/va-icon-overview/va-icon-overview-example.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic icons
+ */
+@Component({
+  selector: 'va-icon-overview-example',
+  templateUrl: 'va-icon-overview-example.html',
+  styleUrls: ['va-icon-overview-example.css'],
+})
+export class VaIconOverviewExample {}
